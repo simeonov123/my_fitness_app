b5938a9d37ef7aad12bb6278c45a8046148c66d6
simeonov123 <simeonov230@gmail.com>
2026-04-13 08:51:37 +0300

    feat: Add import for WorkoutTemplateExerciseSet in TrainingSessionDetailPage

--- flutter-client/mytrainer2client/lib/screens/training_session_detail_page.dart
+++ flutter-client/mytrainer2client/lib/screens/training_session_detail_page.dart
@@ -13,6 +13,7 @@ import '../models/training_session.dart';
 import '../models/workout_instance_exercise.dart';
 import '../models/workout_instance_exercise_set.dart';
 import '../models/workout_template_exercise.dart';
+import '../models/workout_template_exercise_set.dart';
 import '../providers/auth_provider.dart';
 import '../providers/exercise_history_provider.dart';
 import '../providers/exercises_provider.dart';
